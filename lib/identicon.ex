b6ba340c8eb46955b973b44c3ec08bcee3625c31
MightyGoldenOctopus <jean-adrien.ducastaing@epita.fr>
2018-01-03 23:46:12 +0100

    Finished first part, graphical part using egd ahead

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   @doc """
@@ -42,7 +43,7 @@ defmodule Identicon do
 
   @doc """
   Takes an `Identicon.Image{}` and set it's `grid` attribute to an array of tuples.
-  Each tuple `{x, y}` composed with a value `x` and an index `y`.
+  Each tuple `{x, y}` composed with a code `x` and an index `y`.
   Returns an `Identicon.Image{}`.
 
   ## Examples
@@ -75,5 +76,23 @@ defmodule Identicon do
   def mirror_row([first, second | _tail] = row) do
     # append 'second' and 'first' in the 'row' list
     row ++ [second, first]
+  end
+
+  @doc """
+  Takes an `Identicon.Image{}` and filter it's `grid` attribute to remove all element which
+  code value is an odd number. Returns an `Identicon.Image{}`.
+
+  ## Examples
+
+      iex> Identicon.filter_odd_squares(%Identicon.Image{color: {114, 179, 2}, grid: [{114, 0}, {179, 1}, {2, 2}, {179, 3}, {114, 4}, {191, 5}, {41, 6}, {122, 7}, {41, 8}, {191, 9}, {34, 10}, {138, 11}, {117, 12}, {138, 13}, {34, 14}, {115, 15}, {1, 16}, {35, 17}, {1, 18}, {115, 19}, {239, 20}, {239, 21}, {124, 22}, {239, 23}, {239, 24}], hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]})
+      %Identicon.Image{color: {114, 179, 2}, grid: [{114, 0}, {2, 2}, {114, 4}, {122, 7}, {34, 10}, {138, 11}, {138, 13}, {34, 14}, {124, 22}], hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
+  """
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    # syntaxic sugar: function parenthesis can be removed
+    grid = Enum.filter grid, fn({code, _index}) ->
+      # code % 2 (modulo)
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
   end
 end
