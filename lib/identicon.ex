51e6095a6709dcc1f179ae94a04742b02036154d
MightyGoldenOctopus <jean-adrien.ducastaing@epita.fr>
2018-01-04 00:30:30 +0100

    Finished graphical part

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   @doc """
@@ -94,5 +97,47 @@ defmodule Identicon do
       rem(code, 2) == 0
     end
     %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+  Takes an `%Identicon.Image{}` and set `pixel_map` attribute to an array of nested tuples,
+  each containing 2 sets of 2-D coordinates `{{x1,y1}, {x2,y2}}`.
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      # remainder of index / 5 euclidian division
+      horizontal = rem(index, 5) * 50
+      # quotient of index / 5
+      vertical = div(index, 5) * 50
+
+      top_left      = {horizontal, vertical}
+      bottom_right  = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  @doc """
+  Drawing a 250x250px image using egd erlang library
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    # if we don't need reference to original arg we don't have to add the '= image'
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    # exceptionnal case of modifying by object reference, mainly specific to egd library
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  @doc """
+  Save a rendered image on hard drive nammed by the original text input
+  """
+  def save_image(image, filename) do
+    File.write("#{filename}.png")
   end
 end
